376ba8ba4d5d03b1cebf3598dcdbd28ec2c73b39
Renan Souza <renantrsouza@gmail.com>
2023-05-19 16:57:17 -0400

    add: Text component

---
+++ web/src/components/Text.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react'
+import { cva, VariantProps } from 'class-variance-authority'
+
+const textStyles = cva('font-sans text-gray-100', {
+  variants: {
+    size: {
+      lg: 'text-lg',
+      md: 'text-base',
+      sm: 'text-sm',
+    },
+    leading: {
+      relaxed: 'leading-relaxed',
+      snug: 'leading-snug',
+    },
+  },
+})
+
+interface TextProps extends VariantProps<typeof textStyles> {
+  children: ReactNode
+  className?: string
+}
+
+export function Text({
+  children,
+  size = 'lg',
+  leading = 'relaxed',
+  className,
+}: TextProps) {
+  return (
+    <p className={`${textStyles({ size, leading })} ${className}`}>
+      {children}
+    </p>
+  )
+}
