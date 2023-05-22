2b2ca09c592b5fb2658b3dd57a0419de2e7d8145
Renan Souza <renantrsouza@gmail.com>
2023-05-22 11:49:05 -0400

    add: Import fonts in the app

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,9 +1,27 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
+    <View className="flex-1 items-center justify-center bg-zinc-900">
       <Text className="text-5xl font-bold text-gray-50">NLW Spacetime</Text>
       <StatusBar style="light" translucent />
     </View>
