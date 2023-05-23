a414f0786443139ee49575f6d79cf7a35196caae
Renan Souza <renantrsouza@gmail.com>
2023-05-23 13:28:31 -0400

    add: Import logo and add elements for home

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { styled } from 'nativewind'
 
 import {
@@ -10,6 +10,7 @@ import {
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import bgBlur from './src/assets/bg-blur.png'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
 import Stripes from './src/assets/stripes.svg'
 const StyledStripes = styled(Stripes)
 
@@ -27,10 +28,36 @@ export default function App() {
   return (
     <ImageBackground
       source={bgBlur}
-      className="relative flex-1 items-center bg-zinc-900"
+      className="relative flex-1 items-center bg-zinc-900 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Your time capsule
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Collect the memorable moments of your journey and share (if you
+            like) with the world!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          className="rounded-full bg-green-500 px-5 py-2"
+          activeOpacity={0.7}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Register your memory
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Made with 💜 in the Rocketseat&apos;s NLW
+      </Text>
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
