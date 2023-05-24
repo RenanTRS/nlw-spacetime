bcd22fc2ad88b3308893c89e1f601d6bfb12d50c
Renan Souza <renantrsouza@gmail.com>
2023-05-24 13:38:02 -0400

    feat: Add copyright component

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Made with 💜 in the{' '}
+      <a
+        href="https://rocketseat.com.br"
+        rel="noreferrer"
+        target="_blank"
+        className="underline transition-colors hover:text-gray-100"
+      >
+        Rocketseat&apos;s
+      </a>{' '}
+      NLW
+    </div>
+  )
+}
