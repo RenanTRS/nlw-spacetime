9ddd58653b297fa225b9589076da556a15c91df6
Renan Souza <renantrsouza@gmail.com>
2023-05-29 12:18:23 -0400

    add: register for jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,16 @@
 import 'dotenv/config'
 
 import fastify from 'fastify'
+import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
+app.register(jwt, {
+  secret: '72af69df-2f08-4d56-864a-f46e2b313f3c',
+})
 app.register(cors, {
   origin: true,
 })
